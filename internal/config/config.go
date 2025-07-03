05f30a64326940b9ea51864541ab9437f438ba49
codebytecs <va.pogoreloff@gmail.com>
2025-07-03 10:49:45 +0700

    Made config

---
+++ internal/config/config.go
@@ -0,0 +1,33 @@
+package config
+
+type Config struct {
+	Env string `yaml:"env" env-default:"local"`
+	StoragePath string `yaml:"storage_path" env-required:"true"`
+	HTTPserver HTTPserver `yaml:"http_server"`
+}
+
+type HTTPserver struct {
+	Address string `yaml:"address" env-default:"localhost:8082"`
+	Timeout string `yaml:"timeout" env-default:"4s"`
+	IdleTimeout string `yaml:"idle_timeout" env-default:"60s"`
+}
+
+func MustLoad() *Config{
+	configPath := os.Getenv("CONFIG_PATH")
+	if configPath == "" {
+		log.Fatal("CONFIG_PATH is not set")
+	}
+	// check if file exists
+	if _, err := os.Stat(configPath); os.IsNotExist(err) {
+		log.Fatalf("config file does not exist: %s", configPath)
+	}
+	
+	var cfg Config
+
+	if err := cleanenv.ReadConfig(configPath, &cfg); err != nil {
+		log.Fatalf("cannot read config: %s", err)
+	}
+
+	return &cfg
+	
+}
